3a46a28089872c7934acfe501683c81a028d8984
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-13 17:38:29 -0300

    Update 08 TOP WITH TIES.sql

--- Scripts/02. Seleção e filtragem de dados/08 TOP WITH TIES.sql
+++ Scripts/02. Seleção e filtragem de dados/08 TOP WITH TIES.sql
@@ -1,12 +1,12 @@
 USE ContosoRetailDW
 
 SELECT TOP 100 WITH TIES
-	EmailAddress
-	,Gender
-	,MaritalStatus
-	,TotalChildren
-	,NumberChildrenAtHome
-	,NumberCarsOwned AS 'Quantidade Carros'
-	,Education
+	EmailAddress,
+	Gender,
+	MaritalStatus,
+	TotalChildren,
+	NumberChildrenAtHome,
+	NumberCarsOwned AS 'Quantidade Carros',
+	Education
 FROM DimCustomer
 ORDER BY 2 DESC, 3 DESC, 4 DESC, [Quantidade Carros] DESC
