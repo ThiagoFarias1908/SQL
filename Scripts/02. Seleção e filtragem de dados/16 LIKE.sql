62868a0e83abdd0ad5afab4d387ab6b1398d898b
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-13 17:44:23 -0300

    Update 16 LIKE.sql

--- Scripts/02. Seleção e filtragem de dados/16 LIKE.sql
+++ Scripts/02. Seleção e filtragem de dados/16 LIKE.sql
@@ -2,49 +2,49 @@ USE ContosoRetailDW
 
 --Buscando nome completo
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE 'Aaron Painter'
 
---Contém Paul em qualquer parte da string
+--ContÃ©m Paul em qualquer parte da string
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE '%Paul%'
 
 --Termina com Allen
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE '%Allen'
 
---Começa com Michael
+--ComeÃ§a com Michael
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE 'Michael%'
 
---Começa com Nur__
+--ComeÃ§a com Nur__
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE 'Nur__%'
 
 --Termina com Nev__
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE '%nev__'
 
---Contém com gon[zs]ale[zs] 's' ou 'z'
+--ContÃ©m com gon[zs]ale[zs] ===>>> [sz] 's' ou 'z'
 SELECT 
-	FirstName + ' ' + LastName AS 'FullName'
-	,*
+	FirstName + ' ' + LastName AS 'FullName',
+	*
 FROM DimEmployee
 WHERE FirstName + ' ' + LastName LIKE '%gon[zs]ale[zs]%'
