02954141c34e7ca40ce592b63924dea33fd0b606
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-13 17:37:25 -0300

    Update 06 ORDER BY posição ou alias.sql

--- Scripts/02. Seleção e filtragem de dados/06 ORDER BY posição ou alias.sql
+++ Scripts/02. Seleção e filtragem de dados/06 ORDER BY posição ou alias.sql
@@ -1,13 +1,13 @@
 USE ContosoRetailDW
 
 SELECT 
-	EmailAddress
-	,Gender
-	,MaritalStatus
-	,TotalChildren
-	,NumberChildrenAtHome
-	,NumberCarsOwned AS 'Quantidade Carros'
-	,Education
+	EmailAddress,
+	Gender,
+	MaritalStatus,
+	TotalChildren,
+	NumberChildrenAtHome,
+	NumberCarsOwned AS 'Quantidade Carros',
+	Education
 FROM DimCustomer
 --ORDER BY Gender DESC, MaritalStatus DESC, TotalChildren DESC
 ORDER BY 2 DESC, 3 DESC, 4 DESC, [Quantidade Carros] DESC
