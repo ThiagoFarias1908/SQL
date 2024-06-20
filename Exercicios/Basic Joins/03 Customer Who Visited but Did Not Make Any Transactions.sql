5215d85e4903f6044bb08ded6514d2090a20f6d8
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-20 19:51:00 -0300

    Customer Who Visited but Did Not Make Any Transactions

--- Exercicios/Basic Joins/03 Customer Who Visited but Did Not Make Any Transactions.sql
+++ Exercicios/Basic Joins/03 Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,9 +1,9 @@
 SELECT
     customer_id,
-    COUNT(Visits.visit_id) AS count_no_trans
-FROM Visits
-LEFT JOIN Transactions ON Transactions.visit_id = Visits.visit_id
-WHERE Transactions.transaction_id IS NULL
+    COUNT(V.visit_id) AS count_no_trans
+FROM Visits AS V
+LEFT JOIN Transactions AS T ON T.visit_id = V.visit_id
+WHERE T.transaction_id IS NULL
 GROUP BY customer_id
 
 /*
