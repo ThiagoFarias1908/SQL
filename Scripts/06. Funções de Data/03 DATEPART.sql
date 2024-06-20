dfd6734faa0f99edcd2523bc9b402b4c8365997b
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-20 00:05:13 -0300

    Funções de Data

--- Scripts/06. Funções de Data/03 DATEPART.sql
+++ Scripts/06. Funções de Data/03 DATEPART.sql
@@ -3,25 +3,25 @@ USE ContosoRetailDW
 --DATEPART (parte, data)
 /*
 	===================================================================
-		Valor			||	Parte Retornada				||	Abreviação
+		Valor			||	Parte Retornada				||	AbreviaÃ§Ã£o
 	===================================================================
-		year			||	Ano							||	yy, yyyy
-		quarter			||	Trimestre (1/4 de ano)		||	qq, q
-		month			||	Mês							||	mm, m
-		dayofyear		||	Dia do ano					||	dy, y
-		day				||	Dia							||	dd, d
-		week			||	Semana						||	wk, ww
+		year			||	Ano					||	yy, yyyy
+		quarter			||	Trimestre (1/4 de ano)			||	qq, q
+		month			||	MÃªs					||	mm, m
+		dayofyear		||	Dia do ano				||	dy, y
+		day				||	Dia				||	dd, d
+		week			||	Semana					||	wk, ww
 		weekday			||	Dia da semana				||	dw
-		hour			||	Hora						||	hh
-		minute			||	Minuto						||	mi, n
-		second			||	Segundo						||	ss, s
+		hour			||	Hora					||	hh
+		minute			||	Minuto					||	mi, n
+		second			||	Segundo					||	ss, s
 	===================================================================
 */
 
 SELECT TOP 100
 	S.DateKey,
 	DATEPART(YEAR, S.DateKey) AS ANO,
-	DATEPART(MONTH, S.DateKey) AS MÊS,
+	DATEPART(MONTH, S.DateKey) AS MÃŠS,
 	DATEPART(DAY, S.DateKey) AS DIA,
 	DATEPART(DAYOFYEAR, S.DateKey) AS DIADOANO,
 	DATEPART(WEEKDAY, S.DateKey) AS DIADASEMANA
