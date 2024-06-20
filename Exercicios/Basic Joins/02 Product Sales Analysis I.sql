f0faa29cfd4f80a8f5073a2948f3cb08c0126d36
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-20 19:50:04 -0300

    Product Sales Analysis I

--- Exercicios/Basic Joins/02 Product Sales Analysis I.sql
+++ Exercicios/Basic Joins/02 Product Sales Analysis I.sql
@@ -2,8 +2,8 @@ SELECT
     product_name,
     year,
     price
-FROM Sales
-INNER JOIN Product ON Product.product_id = Sales.product_id
+FROM Sales AS S
+INNER JOIN Product AS P ON P.product_id = S.product_id
 
 /*
 Table: Sales
