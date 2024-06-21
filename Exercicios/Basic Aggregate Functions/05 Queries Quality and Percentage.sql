4a1b62915231756796ad771d870f08c6c26758d5
Thiago Farias Lourenço <thiagofarias1908@gmail.com>
2024-06-20 22:44:55 -0300

    Queries Quality and Percentage

--- Exercicios/Basic Aggregate Functions/05 Queries Quality and Percentage.sql
+++ Exercicios/Basic Aggregate Functions/05 Queries Quality and Percentage.sql
@@ -1,8 +1,9 @@
 SELECT
     query_name,
-    ROUND(AVG(CAST(rating AS decimal) / position), 2) AS quality,
-    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(AVG(IF(rating < 3, 1,0)) * 100, 2) AS poor_query_percentage
 FROM Queries
+WHERE query_name IS NOT NULL
 GROUP BY query_name
 
 /*
